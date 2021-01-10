95f48cb40acf5d252e93a67f5652f7df7657b8ad
firdauslazim <firdauslazim@gmail.com>
2021-01-10 08:55:47 +0800

    initial commit

---
+++ basic_python/helloPython.py
@@ -0,0 +1,15 @@
+import datetime
+
+currDate = datetime.datetime.now()
+print('the time is : ', currDate)
+print('hello world my first python, wlcome firdaus')
+
+# string, int , float eg:
+
+x = 10
+y = "koko"
+
+sum1 = x+x
+sum2 = y+y
+print(sum1,sum2)
+print(y.upper())
